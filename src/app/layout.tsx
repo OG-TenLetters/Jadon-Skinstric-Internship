3c34f32996e0756eb2aae38c5a42b7a5fd1d4dab
Jadon <1tenletters0@gmail.com>
2025-05-19 08:08:10 -0700

    I finished making the intro page with full responsiveness. I will be moving onto the second page after PR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${notoSans.className} antialiased`}>
         <nav className="flex items-center h-[7vh] text-xs justify-between">
-          <div className="flex gap-4 pl-8 pr-8  tracking-wide">
+          <div className="flex gap-4 sm:px-8 pl-4 tracking-wide">
             <div className="font-bold">sKINsTRIC</div>
             <div className="text-gray-500">[ INTRO ]</div>
           </div>
-          <button className="text-white py-2 px-4 mx-8 bg-black uppercase font-bold rounded-xs
+          <button className="text-white py-2 px-4 sm:mx-8 mr-4 bg-black uppercase font-bold rounded-xs
            transition-all duration-300 hover:shadow-[0_2px_12px_4px_rgba(0,0,0,0.15)] hover:-translate-y-0.5 hover:active:scale-100 hover:active:shadow-none cursor-not-allowed">
             Enter Code
           </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const sophisticatedTextRef = useRef<HTMLDivElement>(null);
   const leftDiamondRef = useRef<HTMLDivElement>(null);
   const rightDiamondRef = useRef<HTMLDivElement>(null);
-  const spanRef =  useRef<HTMLDivElement>(null);
+  const spanRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const lT = leftTriggerRef.current;
@@ -21,8 +21,8 @@ export default function Home() {
       const handleLeftHoverEnter = () => {
         sText.style.transition = "all 0.6s ease-in-out";
         sText.style.transform = "translateX(15vw)";
-        sp.style.transition = 'all 0.6s ease-in-out'
-        sp.style.transform = 'translateX(7rem)'
+        sp.style.transition = "all 0.6s ease-in-out";
+        sp.style.transform = "translateX(6rem)";
         rT.style.transition = "opacity 0.6s ease-in-out";
         rT.style.opacity = "0";
         rD.style.transition = "opacity 0.6s ease-in-out";
@@ -33,28 +33,26 @@ export default function Home() {
         sText.style.textAlign = "center";
         rT.style.transition = "opacity 0.6s ease-in-out";
         rD.style.transition = "opcacity 0.6s ease-in-out";
-        sp.style.transform = 'translateX(0)'
+        sp.style.transform = "translateX(0)";
         rT.style.opacity = "1";
         rD.style.opacity = "1";
-
       };
       const handleRightHoverEnter = () => {
         sText.style.transition = "all 0.6s ease-in-out";
         sText.style.transform = "translateX(-15vw)";
         lT.style.transition = "opacity 0.6s ease-in-out";
         lD.style.transition = "opacity 0.6s ease-in-out";
-        sp.style.transition = 'all 0.6s ease-in-out'
-        sp.style.transform = 'translateX(-7rem)'
+        sp.style.transition = "all 0.6s ease-in-out";
+        sp.style.transform = "translateX(-6rem)";
         lT.style.opacity = "0";
         lD.style.opacity = "0";
       };
       const handleRightHoverLeave = () => {
         sText.style.transform = "translateX(0)";
         sText.style.textAlign = "center";
-        sp.style.transform = 'translateX(0)';
+        sp.style.transform = "translateX(0)";
         lT.style.opacity = "1";
         lD.style.opacity = "1";
-
       };
       lT.addEventListener("mouseenter", handleLeftHoverEnter);
       lT.addEventListener("mouseleave", handleLeftHoverLeave);
@@ -71,17 +69,23 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="no-Xscroll flex flex-col h-[93vh] p-4 justify-center ">
-      <div ref={leftDiamondRef} className="absolute w-[60vh] h-[60vh] rotate-45 left-0 translate-x-[-60%] -z-1 border border-gray-400 border-dotted"></div>
-      <div ref={rightDiamondRef} className="absolute w-[60vh] h-[60vh] rotate-45 right-0 -translate-x-[-60%] -z-1 border border-gray-400 border-dotted "></div>
+    <div className="no-Xscroll flex flex-col h-[93vh] p-4 justify-center lg:items-stretch items-center">
+      <div
+        ref={leftDiamondRef}
+        className="absolute md:w-[60vh] md:h-[60vh] w-[40vh] h-[40vh] rotate-45 lg:left-0 lg:translate-x-[-60%] left-[50%] translate-x-[-50%] lg:scale-100 scale-85 -z-1 border border-gray-400 border-dotted"
+      ></div>
+      <div
+        ref={rightDiamondRef}
+        className="absolute md:w-[60vh] md:h-[60vh] w-[40vh] h-[40vh] rotate-45 lg:right-0 lg:-translate-x-[-60%] lg:scale-100 right-[50%] translate-x-[50%] scale-70 -z-1 border border-gray-400 border-dotted "
+      ></div>
 
-      <div className="flex justify-between items-center">
+      <div className="flex lg:justify-between items-center justify-center">
         {/* 3. Attach the ref to the left trigger div */}
         <div
           ref={leftTriggerRef}
-          className="flex items-center gap-4 relative ml-20"
+          className="group lg:flex items-center gap-4 relative ml-20 hidden"
         >
-          <div className="relative flex justify-center items-center p-2 border border-black border-solid rotate-45">
+          <div className="group-hover:scale-110 group-active:scale-95 relative flex justify-center items-center p-2 border border-black border-solid rotate-45 transition-all duration-600  ">
             {/* Assuming Triangle.src is the correct way to get the image URL */}
             <img
               className="w-[12px] h-[12px] -translate-x-[2px] translate-y-[2px] -rotate-225"
@@ -95,19 +99,18 @@ export default function Home() {
         {/* 3. Attach the ref to the target sophisticated text div */}
         <div
           ref={sophisticatedTextRef}
-          className="flex flex-col h-100 justify-center items-center max-w-[800px] text-center text-8xl"
+          className="lg:flex lg:flex-col justify-center items-center lg:max-w-[800px] md:max-w-[600px] max-w-[300px] text-center xl:text-8xl lg:text-7xl sm:max-w-[600px] md:text-6xl text-4xl block"
         >
-          {" "}
-          Sophisticated <span ref={spanRef} >skincare</span>
+          Sophisticated <span ref={spanRef}>skincare</span>
         </div>
 
         {/* 3. Attach the ref to the right trigger div */}
         <div
           ref={rightTriggerRef}
-          className="group flex items-center gap-4 relative mr-20"
+          className="group lg:flex items-center gap-4 relative mr-20 hidden"
         >
           <div className="uppercase">take test</div>
-          <div className="relative flex justify-center items-center p-2 border border-black border-solid rotate-45 ">
+          <div className="group-hover:scale-110 group-active:scale-95 relative flex justify-center items-center p-2 border border-black border-solid rotate-45 transition-all duration-600">
             {/* Assuming Triangle.src is the correct way to get the image URL */}
             <img
               className="w-[12px] h-[12px] translate-x-[2px] -translate-y-[2px] -rotate-45"
@@ -118,9 +121,20 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="uppercase text-xs max-w-[260px] absolute bottom-16 left-16">
-        skinstic developed an a.i. that creates a highly-personalized routine
-        tailored to what your skin needs
+      <div className="lg:uppercase lg:text-black text-[#919292] my-4 sm:text-sm text-xs max-w-[260px] lg:font-normal font-bold lg:absolute lg:bottom-4 lg:left-20 lg:text-left static text-center">
+        Skinstic developed an A.I. that creates a highly-personalized routine
+        tailored to what your skin needs.
+      </div>
+      <div className="hover:scale-105 hover:active:scale-100 lg:hidden items-center gap-4 transition-all duration-300 relative flex">
+        <div className="uppercase font-bold sm:text-sm text-xs">enter experience</div>
+        <div className="relative flex justify-center items-center p-2 border border-black border-solid rotate-45 transition-all duration-600 md:scale-100 sm:scale-90 scale-70">
+          {/* Assuming Triangle.src is the correct way to get the image URL */}
+          <img
+            className="w-[12px] h-[12px] translate-x-[2px] -translate-y-[2px] -rotate-45"
+            src={Triangle.src}
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
